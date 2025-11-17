5717492f7cd8f332be394b2109bc2125879bcf8d
Max Mezalon <43793449+jmezalon@users.noreply.github.com>
2025-11-17 12:05:32 -0500

    Update layout.tsx make room in navbar

--- app/(public)/layout.tsx
+++ app/(public)/layout.tsx
@@ -83,12 +83,11 @@ export default function PublicLayout({
                 <Link href="/profile">
                   <Button variant="ghost" size="sm">
                     <User className="h-4 w-4 mr-2" />
-                    {user.name || user.email}
+                    {user.name.split(" ")[0] || user.email.split(" ")[0][0]}
                   </Button>
                 </Link>
                 <Button variant="ghost" size="sm" onClick={handleLogout}>
                   <LogOut className="h-4 w-4 mr-2" />
-                  Sign Out
                 </Button>
               </>
             ) : (
